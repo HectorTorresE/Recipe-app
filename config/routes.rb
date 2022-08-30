5a178bd752893892c2776832f6a204a419842c09
HectorTorresE <hectorjte@gmail.com>
2022-08-30 09:32:49 -0600

    Add user and recepie table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
 
+  root 'foods#index'
   resources :foods
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
