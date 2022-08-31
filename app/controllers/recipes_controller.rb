f2b903baab94855adecd58663594c3fad31e45e2
HectorTorresE <hectorjte@gmail.com>
2022-08-31 15:51:07 -0600

    Add create recipe and add public field to the recipe table

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,26 @@
 class RecipesController < ApplicationController
   def index
-    @user = User.first
+    @user = current_user
     @recipes = Recipe.where(user: @user)
+  end
+
+  def new
+    @user = current_user
+    @recipe = Recipe.new
+  end
+
+  def create
+    permitted = params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
+    @recipe = Recipe.new(user: current_user, name: permitted[:name], preparation_time: permitted[:preparation_time], cooking_time: permitted[:cooking_time], description: permitted[:description], public?: permitted[:public])
+    if @recipe.valid? && @recipe.save
+      redirect_to recipes_path
+    else
+      render root_path
+    end
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public?)
   end
 
   def show; end
